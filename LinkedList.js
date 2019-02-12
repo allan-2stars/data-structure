a9d1b50afb9739c1332dea68e09b5c5899a635bc
Allan Wang <wang.allan@outlook.com>
2019-02-12 20:23:43 +1100

    create a new data structure Linked List with append and prepend

--- LinkedList.js
+++ LinkedList.js
@@ -22,7 +22,42 @@ class LinkedList {
     this.tail = this.head;
     this.lenght = 1;
   }
+  append(value) {
+    const newNode = {
+      value,
+      next: null
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.lenght++;
+    return this;
+  }
+
+  //// Below also works
+  //   prepend(value) {
+  //     const newNode = {
+  //       value,
+  //       next: this.head
+  //     };
+  //     this.head.next = this.head;
+  //     this.head = newNode;
+  //     this.lenght++;
+  //     return this
+  //   }
+
+  prepend(value) {
+    const newNode = {
+      value,
+      next: null
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.lenght++;
+    return this;
+  }
 }
 
-const myLinkedList = new LinkedList(10);
+const myLinkedList = new LinkedList(7);
+myLinkedList.append(9);
+myLinkedList.prepend(15);
 console.log(myLinkedList);
